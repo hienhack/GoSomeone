d18d1ccaffd57c384507ef1dc853566c7a36df04
20120472 <hienthai.030102@gmail.com>
2023-04-13 12:06:58 +0700

    Add all

---
+++ js/standard.js
@@ -0,0 +1,288 @@
+$(() => {
+    $('.header-white .logo img').attr('src', './assets/logo_b.png');
+    $('.header-white .navbar-menu-item a').css({
+        "color": "var(--text-dark)"
+    });
+
+    $('.header-white').css({
+        "box-shadow": "var(--box-shadow)"
+    });
+
+    $('.input-img input').on({
+        change: function (event) {
+            if (event.target.files.length > 0) {
+                const preview = $(event.target).parent().children('.preview');
+                preview.children('img').remove();
+                for (let i = 0; i < event.target.files.length; i++) {
+                    let src = URL.createObjectURL(event.target.files[i]);
+                    preview.append(`<img src=${src}>`);
+                }
+            }
+        }
+    });
+
+    $('#input-profile-img input').on({
+        change: function (event) {
+            if (event.target.files.length > 0) {
+                $(event.target).parent().children('.preview').removeClass('d-none');
+                $('#input-profile-img img').addClass('img-thumbnail');
+                $('#input-profile-img img').addClass('img-thumbnail');
+            }
+            else {
+                $(event.target).children('.preview').addClass('d-none');
+            }
+        }
+    })
+
+    $('#change-ava').on({
+        change: function (event) {
+            if (event.target.files.length > 0) {
+                let src = URL.createObjectURL(event.target.files[0]);
+                $('#avatar').attr('src', src);
+            }
+        }
+    });
+
+    $('#input-cover-img').on({
+        change: function (event) {
+            if (event.target.files.length > 0) {
+                let src = URL.createObjectURL(event.target.files[0]);
+                $('#cover-img').attr('src', src);
+            }
+        }
+    });
+
+    $('.viewable-img').on({
+        click: function (event) {
+            $('body').addClass('disable-scroll');
+            var doc = document.documentElement;
+            var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+            var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+            $('.img-view').css({
+                top: top,
+                left: left
+            });
+            $('.img-view').removeClass('d-none');
+            $('.img-view').append(`
+                <img src='${event.target.src}'>
+            `)
+        }
+    });
+
+    $('.img-view .btn-close').on({
+        click: function (event) {
+            $('body').removeClass('disable-scroll');
+            $('.img-view').addClass('d-none');
+            $('.img-view img').remove();
+        }
+    });
+
+
+    // Tripview
+
+    $('#trip-imgs').on('slid.bs.carousel', function (event) {
+        const total = $('#trip-imgs .carousel-item').length;
+        const imgs = $('#trip-imgs .carousel-item img');
+        const prev = event.from;
+        const next = (event.from + 2) % total;
+        $('#prev-item').css('background-image', `url(${imgs[prev].src})`);
+        $('#next-item').css('background-image', `url(${imgs[next].src})`);
+    })
+
+
+    // all trips
+    $('.filter-btn').on({
+        click: function (event) {
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+            } else {
+                $(this).addClass('active');
+            }
+        }
+    });
+
+    $('#price').on({
+        input: function (event) {
+            $('#price-from').html(event.target.value + '');
+        }
+    });
+
+    const MONTH = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+    const date = new Date();
+    const year = date.getFullYear();
+    for (let i = date.getMonth(); i < date.getMonth() + 5; i++) {
+        var m = i;
+        var y = year;
+        if (i > 12) {
+            month = i % 12;
+            y = year + 1;
+        }
+        $('#dep-month-from').append(`<option value="${MONTH[m - 1]} ${y}">${MONTH[m]} ${y}</option>`);
+        $('#dep-month-to').append(`<option value="${MONTH[m - 1]} ${y}">${MONTH[m]} ${y}</option>`);
+    }
+
+
+    // create trip
+
+    $(document).on('click', '.increase', function (event) {
+        const input = $($(this).attr('data-bs-target'));
+        const newVal = parseInt(input.val()) + 1;
+        input.attr('value', newVal);
+    });
+
+    $(document).on('click', '.decrease', function (event) {
+        const input = $($(this).attr('data-bs-target'));
+        var newVal = parseInt(input.val()) - 1;
+        if (newVal < 0) {
+            newVal = 0;
+        }
+        input.attr('value', newVal);
+    });
+
+    $(document).on('click', '.input-day .decrease', function (event) {
+        $('#total-days').html(parseInt($('#total-days').html()) - 1);
+    });
+
+    $(document).on('click', '.input-day .increase', function (event) {
+        $('#total-days').html(parseInt($('#total-days').html()) + 1);
+    });
+
+    $(document).on('change', '.stop-card .input-stop-img', function (event) {
+        if (event.target.files.length > 0) {
+            let src = URL.createObjectURL(event.target.files[0]);
+            const id = '#' + $(this).parent().parent().parent()
+                .parent().attr('id');
+            $(`${id} .stop-img`).css('background-image', 'url(' + src + ')');
+            $(`${id} .stop-img`).removeClass('d-none');
+            $(`${id} .input-stop-img`).parent().parent().parent().addClass('d-none');
+        }
+    });
+
+    $(document).on('input', '.input-location', function (event) {
+        const id = parseInt(event.target.id.split('-')[2]);
+        var location = event.target.value;
+        if (location == "") {
+            location = "Stop " + id;
+        }
+
+        $(`#stop-${id} .stop-location`).html(location);
+    }
+    );
+
+    $('#add-stop').on({
+        click: function (event) {
+            const id = $('.stop-card').length + 1;
+            const newStop = `
+                <div id="stop-${id}" class="stop-card bg-light rounded container-md p-0">
+                    <div class="d-flex justify-content-between align-items-center p-3">
+                        <div>
+                            <h5 class="fw-bold mb-1">Add image for stop here</h5>
+                            <span class="text-secondary fs-7">Make sure to upload relevent, high quality image.</span>
+                        </div>
+                        <div>
+                            <label class="btn btn-outline-dark d-flex align-item-center gap-2">
+                                <input id="input-stop-img-${id}" class="d-none input-stop-img" type="file" name="stop_image" required>
+                                <i class="fa-regular fa-image fs-4"></i>
+                                <span class="fw-bold">ADD</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="stop-img rounded-top d-none">
+                        <div class="w-100 h-100 d-flex align-items-end p-3">
+                            <div>
+                                <label for="input-stop-img-${id}" class="btn btn-light">
+                                    <i class="fa-regular fa-image fs-4"></i>
+                                    <span class="fw-bold">Change</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-3">
+                        <div class="d-flex align-items-center gap-3 mb-4">
+                            <h6 class="stop-number">${id}</h6>
+                            <h5 class="stop-location" class="fw-bold">Stop ${id}</h5>
+                        </div>
+                        <div class="d-flex align-items-center gap-3 mb-3">
+                            <div class="form-floating flex-fill">
+                                <input id="input-location-${id}" type="text" class="input-location form-control">
+                                <label>Location</label>
+                            </div>
+                            <div class="input-day d-flex gap-1 align-items-center">
+                                <div class="d-flex flex-column">
+                                    <div class="increase text-secondary border btn btn-sm" data-bs-target="#stop-day-${id}">
+                                        <i class="fa-solid fa-caret-up"></i>
+                                    </div>
+                                    <div class="decrease text-secondary border btn btn-sm" data-bs-target="#stop-day-${id}">
+                                        <i class="fa-solid fa-caret-down"></i>
+                                    </div>
+                                </div>
+                                <div class="border p-2 px-4 fs-3 fw-bold rounded-1">
+                                    <input id="stop-day-${id}" type="number" class="text-center" name="total_days" value="1">
+                                    <span class="fw-bold fs-5">day(s)</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-floating mb-1">
+                            <textarea name="description" class="form-control h-100" cols="40" rows="5"></textarea>
+                            <label>Tell us more</label>
+                        </div>
+                        <span class="text-secondary fs-7">What do you do / visit in this place</span>
+                    </div>
+                </div>
+            `
+            $(`#stop-${id - 1}`).after(newStop);
+        }
+    });
+
+    $(document).on('change', '#stops input', function (event) {
+        const inputed = $('.input-day input');
+        var total = 0;
+        for (let i = 0; i < inputed.length; i++) {
+            total += parseInt(inputed[i].value);
+        }
+        $('#total-days').html(total);
+    });
+
+    $('#input-trip-imgs').on({
+        change: function (event) {
+            if (event.target.files.length > 0) {
+                for (let i = 0; i < event.target.files.length; i++) {
+                    let src = URL.createObjectURL(event.target.files[i]);
+                    $('#trip-imgs').removeClass('d-none');
+                    $('#trip-imgs .carousel-inner').append(`
+                        <div class="carousel-item active">
+                            <img src="${src}" class="d-block">
+                        </div>`
+                    );
+                }
+            }
+            else {
+                $('#trip-imgs').addClass('d-none');
+            }
+        }
+    });
+
+
+    // my trip
+    $('#route button').on({
+        click: function(event) {
+            $('#route button').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+    $('#pending-btn').on({
+        click: function(event) {
+            $('#pending-trips').removeClass('d-none');
+            $('#created-trips').addClass('d-none');
+        }
+    });
+
+    $('#created-btn').on({
+        click: function(event) {
+            $('#created-trips').removeClass('d-none');
+            $('#pending-trips').addClass('d-none');
+        }
+    });
+})
---
+++ test.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
+const hbs_helper = require("./helpers/hbs_helper")
+
+// Dùng file .env
+dotenv.config({ path: __dirname + '/.env' });
+
+// Connect Database
+mongoose.set('strictQuery', false);
+mongoose.connect((process.env.MONGODB_URL), () => {
+    console.log("Đã kết nối đến MongoDB");
+});
+
+const siteR=require('./routers/site.router');
+const tripR=require('./routers/tripSite.router');
+const { userInfo } = require('os');
+
+
+const app = express();
+const port = 3000;
+const localhost = '127.0.0.1';
+
+//Set up session
+require('./config/session')(app)
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//cookie
+app.use(cookieParser());
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    helpers: hbs_helper
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/site',siteR);
+app.use('/trip',tripR)
+
+// app.get('/', (req, res) => {
+//     if (req.cookies.token) {
+//         res.render('home', {
+//             user: req.cookies.user
+//         });
+//     }else {
+//         res.render('home');
+//     }
+   
+// });
+// app.get('/', (req, res) => {
+//     res.render('trip', {
+//         author: true,
+//         request: [
+//             {
+//                 user: {
+//                     _id: "34234",
+//                     first_name: "Waldez",
+//                     ava: "https://st.nhipcaudautu.vn/staticFile/Subject/2021/01/22/gioi-thieu-ve-steve-jobs-tl_22161484.jpg"
+//                 },
+//             },
+//             {
+//                 user: {
+//                     _id: "34234",
+//                     first_name: "Waldez",
+//                     ava: "https://st.nhipcaudautu.vn/staticFile/Subject/2021/01/22/gioi-thieu-ve-steve-jobs-tl_22161484.jpg"
+//                 },
+//             }
+//         ]
+//     })
+// })
+
+app.get('/', (req, res) => {
+    res.render('trip', {
+        joined: true,
+        user: {
+            _id: "adsfad",
+
+        }
+    })
+});
+
+
+app.use((req, res, next) => {
+    next(new Error('Page not found'));
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode | 500;
+    res.status(statusCode)
+        .send(err.message);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://${localhost}:${port}/`);
+});
